5a0f15976f434dc1eee901308f516daeecdb7338
felipericarte <felipericartem@gmail.com>
2025-03-18 18:33:50 -0300

    commit inicial da estrutura

---
+++ src/main/java/flow/com/bevflow/BevflowApplication.java
@@ -0,0 +1,13 @@
+package flow.com.bevflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BevflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BevflowApplication.class, args);
+    }
+
+}
